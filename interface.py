50f9bb5e4caf5e5a6091668f9556aa9165ecf173
JanezTheProgramer <43420276+JanezTheProgramer@users.noreply.github.com>
2019-03-16 15:21:50 +0100

    even more code formating it's becoming to much

--- interface.py
+++ interface.py
@@ -3,8 +3,7 @@ from kivy.app import App
 from kivy.uix.stacklayout import StackLayout
 from kivy.core.window import Window
 
-Window.size = (1280, 640)
-Window.clearcolor = (0.2, 0.2, 0.2, 0.5)
+[setattr(Window, x, y) for x, y in {"size": tuple((1280, 640)), "clearcolor": tuple((0.2, 0.2, 0.2, 0.5))}.items()]
 
 class Controller(StackLayout):
     def __init__(self):
@@ -13,14 +12,12 @@ class Controller(StackLayout):
     def btn_click(self, btn, slider): slider.value = 100.00 if "ON" not in btn.text.upper() else 0.00
 
     def slider_drag(self, btn, slider):
-        if slider.value > 1.00:
-            btn.text=("[color=888][b]ON[/b][/color]")
-            btn.background_color=(.2, .8, .3, 1.0)
-        else:
-            btn.text=("[color=888][b]OFF[/b][/color]")
-            btn.background_color=(.7, .7, .7, 1.0)
+            [setattr(btn, x, y) for x, y in {
+                "text": "[color=888][b]ON[/b][/color]" if slider.value > 1.00 else "[color=888][b]OFF[/b][/color]", 
+                "background_color": (.2, .8, .3, 1.0) if slider.value > 1.00 else (.7, .7, .7, 1.0)
+            }.items()]
 
-class InterfaceApp(App):
+class InterfaceApp (App): 
     build = lambda self: Controller()
 
 InterfaceApp().run() if __name__ == '__main__' else None
